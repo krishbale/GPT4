b13e29e321bae5b4c81697ca0440370de32ad8f5
balkrishna pokharel <sushil.pokharel.7528@gmail.com>
2023-05-04 13:23:40 +0545

    featureadded

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import './brand.css';
+import { google, slack,atlassian,dropbox,shopify
+} from './import';
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='gpt3__brand section__padding' >
+   <div>
+    <img src={google} alt="" />
+   </div>
+   <div>
+    <img src={slack} alt="" />
+   </div>
+   <div>
+    <img src={atlassian} alt="" />
+   </div>
+   <div>
+    <img src={dropbox} alt="" />
+   </div>
+   <div>
+    <img src={shopify} alt="" />
+   </div>
+
+    </div>
   )
 }
 
